e6c9357a319bf544c312241d58b744dba760485e
MarcusNg205 <161159606+MarcusNg205@users.noreply.github.com>
2024-02-25 11:52:13 -0500

    Delete Hackathon directory

--- Hackathon/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Feature
-from .models import FormData
-
-# Register your models here.
-admin.site.register(Feature)
-
-admin.site.register(FormData)
--- Hackathon/myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import FormData
-
-class FormDataForm(forms.ModelForm):
-    class Meta:
-        model = FormData
-        fields = ['name', 'email', 'uploaded_file']
