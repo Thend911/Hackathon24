111c40fccdb379d82fb3c94cff81dd495875d76c
MarcusNg205 <161159606+MarcusNg205@users.noreply.github.com>
2024-02-25 11:54:44 -0500

    Add files via upload

---
+++ myapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.service, name='services'),
+    path('contact', views.contact, name='contact'),
+    path('register', views.register, name='register'),
+    path('output', views.output, name='output'),
+    path('upload_file', views.upload_file, name='upload_file'),
+    path('login', views.login, name='login')
+]
+
+
+
